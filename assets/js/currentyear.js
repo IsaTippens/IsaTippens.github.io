06654b621d73d0d2f6f21c7d3a5aead9a2915132
IsaTippens <isatippens2@gmail.com>
2022-02-01 19:20:03 +0200

    Changed fonts. Numbers count themselves

--- assets/js/currentyear.js
+++ assets/js/currentyear.js
@@ -6,8 +6,24 @@ function setYear() {
         document.getElementById("year").innerHTML = year
 }
 
+//https://stackoverflow.com/a/21984136
+function getYearsSince(day, month, year) {
+    var ageDifMs = Date.now() - new Date(year, month, day).getTime();
+    var ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+function setAge() {
+    document.getElementById("age").innerHTML = getYearsSince(1, 6, 2001)
+}
+
+function setCoding() {
+    document.getElementById("coding").innerHTML = getYearsSince(1, 1, 2013)
+}
+
 function loadItAll() {
-    setYear()
+    //setYear();
+    setAge();
+    setCoding();
 }
 
 window.onload = loadItAll
