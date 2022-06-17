81d544e48f7c30b5cc9a8f4e2db6caa51f787d39
Isa Tippens <37983077+IsaTippens@users.noreply.github.com>
2022-06-18 01:07:50 +0200

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -8,7 +8,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'IT',
       theme: lightTheme,
       darkTheme: darkTheme,
 
